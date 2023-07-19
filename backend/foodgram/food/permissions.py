218d44a48b15bc24fa0debd2e0d7728cd38534ea
Nikita Kolesnik <kolesniknikita21@yandex.ru>
2023-07-19 15:12:00 +0400

    Correct permissions

--- backend/foodgram/food/permissions.py
+++ backend/foodgram/food/permissions.py
@@ -26,18 +26,3 @@ class AdminOrReadOnly(BasePermission):
             or request.user.is_authenticated
             and request.user.is_staff
         )
-
-
-class OwnerUserOrReadOnly(BasePermission):
-    """
-    Разрешение на создание и изменение только для админа и пользователя.
-    Остальным только чтение объекта.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        return (
-            request.method in SAFE_METHODS
-            or request.user.is_authenticated
-            and request.user == obj.author
-            or request.user.is_staff
-        )
